544380607e83726f4f2c3ae95f4693dfac70a93e
Theodore Smith Scott <theosmithscott@protonmail.com>
2023-03-16 18:30:34 +0000

    Added image upload to backend

--- web/app.py
+++ web/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session, request
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    image = request.files['raw_image']
+    # Process image using model and return result
+    return 'Successfully uploaded image'
+    
