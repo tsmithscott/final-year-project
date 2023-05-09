48e2b3e4d0f517f079822c585b57a27c9f94c752
Theodore Smith Scott <theosmithscott@protonmail.com>
2023-05-09 23:42:34 +0100

    Added defective status output - Started adding processed image response

--- web/app.py
+++ web/app.py
@@ -2,8 +2,9 @@ import os
 import threading
 import time
 import uuid
+from io import BytesIO
 
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for, make_response
 from localization import is_defective, save_activation_map
 from PIL import Image
 
@@ -20,9 +21,9 @@ def garbage_collection(path: str, time_seconds: int):
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', defective=None)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/process-image', methods=['POST'])
 def upload():
     if not request.files['raw_image']:
         return 'No file uploaded'
@@ -36,18 +37,26 @@ def upload():
         output_image_path = os.path.join('tmp', f'{file_name}-output.png')
         image.save(image_path)
         
-        # Check if the image is defective + delete the image after 30 seconds
+        # Check if the image is defective + delete the image after 15 seconds
         defective = is_defective(image_path)
-        threading.Thread(target=garbage_collection, args=(image_path, 30), daemon=True).start()
-
-        # Plot and save the output image to the tmp folder + delete the image after 30 seconds
-        save_activation_map(image_path)
-        threading.Thread(target=garbage_collection, args=(output_image_path, 30), daemon=True).start()
+        threading.Thread(target=garbage_collection, args=(image_path, 15), daemon=True).start()
         
         if defective:
-            print('Defective')
-            return 'Defective'
+            print("Defective image detected")
+            save_activation_map(image_path)
+            # Plot and save the output image to the tmp folder + delete the image after 60 seconds
+            threading.Thread(target=garbage_collection, args=(output_image_path, 60), daemon=True).start()
+            
+            with Image.open(output_image_path) as img:
+                image_data = BytesIO()
+                img.save(image_data, format='PNG')
+                image_data.seek(0)
+                
+            response = make_response(image_data.getvalue())
+            response.headers.set('Content-Type', 'image/png')
+            response.headers.set('Content-Disposition', 'attachment', filename='processed_image.png')
+            return response
         else:
-            print('Not Defective')
-            return 'Not Defective'
+            print("Non-defective image detected")
+            return make_response("", 204)
     
