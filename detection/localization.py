0d7718a0eef01d6ffd332511d1a81edbd50162e1
Theodore Smith Scott <theosmithscott@protonmail.com>
2023-05-09 17:08:24 +0100

    Defective-only heatmaps

--- detection/localization.py
+++ detection/localization.py
@@ -140,33 +140,55 @@ model = load_trained_model()
 print(classification_report(label_test, np.argmax(model.predict(test_generator),axis=1)))
 
 
-def plot_activation():
+def is_defective(image):
+    # Make a prediction on the image using the model
+    pred = model.predict(image[np.newaxis,:,:,:])
     
-    fig, axes = plt.subplots(nrows=len(X), ncols=2, figsize=(10, 10))
+    # Check if the predicted class is 0 (defective)
+    if np.argmax(pred) == 0:
+        return True
+    else:
+        return False
 
+
+def plot_activation():
+    # Count the number of defective images
+    num_defective = sum([is_defective(img) for img in X])
+    
+    # Determine the number of rows based on the number of defective images
+    num_rows = num_defective
+    
+    # Create the subplots
+    fig, axes = plt.subplots(nrows=num_rows, ncols=2, figsize=(8, 8))
+    
+    # Initialize a counter for the row index
+    row_index = 0
+    
+    # Iterate over the images
     for i, img in enumerate(X):
-        pred = model.predict(img[np.newaxis,:,:,:])
-        pred_class = np.argmax(pred)
+        if is_defective(img):
+            weights = model.layers[-1].get_weights()[0]
+            class_weights = weights[:, 0]
 
-        weights = model.layers[-1].get_weights()[0]
-        class_weights = weights[:, pred_class]
+            intermediate = tf.keras.Model(model.input, model.get_layer("block5_conv3").output)
+            conv_output = intermediate.predict(img[np.newaxis,:,:,:])
+            conv_output = np.squeeze(conv_output)
 
-        intermediate = tf.keras.Model(model.input, model.get_layer("block5_conv3").output)
-        conv_output = intermediate.predict(img[np.newaxis,:,:,:])
-        conv_output = np.squeeze(conv_output)
+            h = int(img.shape[0]/conv_output.shape[0])
+            w = int(img.shape[1]/conv_output.shape[1])
 
-        h = int(img.shape[0]/conv_output.shape[0])
-        w = int(img.shape[1]/conv_output.shape[1])
+            activation_maps = sp.ndimage.zoom(conv_output, (h, w, 1), order=1)
+            out = np.dot(activation_maps.reshape((img.shape[0]*img.shape[1], 512)), class_weights).reshape(
+                img.shape[0],img.shape[1])
 
-        activation_maps = sp.ndimage.zoom(conv_output, (h, w, 1), order=1)
-        out = np.dot(activation_maps.reshape((img.shape[0]*img.shape[1], 512)), class_weights).reshape(
-            img.shape[0],img.shape[1])
-
-        axes[i][0].imshow(img.astype('float32').reshape(img.shape[0],img.shape[1],3))
-        axes[i][1].imshow(img.astype('float32').reshape(img.shape[0],img.shape[1],3))
-        axes[i][1].imshow(out, cmap='jet', alpha=0.35, extent=[0, img.shape[1], img.shape[0], 0])
-        axes[i][0].set_title('Not Defective' if pred_class == 1 else 'Defective')
-        axes[i][1].set_title('Activation Map')
+            axes[row_index][0].imshow(img.astype('float32').reshape(img.shape[0],img.shape[1],3))
+            axes[row_index][1].imshow(img.astype('float32').reshape(img.shape[0],img.shape[1],3))
+            axes[row_index][1].imshow(out, cmap='jet', alpha=0.35, extent=[0, img.shape[1], img.shape[0], 0])
+            axes[row_index][0].set_title('Defective')
+            axes[row_index][1].set_title('Activation Map')
+            
+            # Increment the row index
+            row_index += 1
 
     plt.tight_layout()
     plt.show()
