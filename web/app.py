b0a765b516c1116743458fd0e9345bac15414503
Theodore Smith Scott <theosmithscott@protonmail.com>
2023-05-09 20:35:18 +0100

    Defective check implemented

--- web/app.py
+++ web/app.py
@@ -1,7 +1,13 @@
+import uuid
+
 from flask import Flask, render_template, redirect, url_for, session, request
+from PIL import Image
+
+from localization import load_trained_model, is_defective, save_activation_map
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+model = load_trained_model()
 
 @app.route('/', methods=['GET'])
 def index():
@@ -9,7 +15,16 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    image = request.files['raw_image']
-    # Process image using model and return result
-    return 'Successfully uploaded image'
+    if not request.files['raw_image']:
+        return 'No file uploaded'
+    else:
+        image = request.files['raw_image']
+        image_path = f"tmp/{str(uuid.uuid4().hex)}.jpeg"
+        image.save(image_path)
+        if is_defective(image_path) == True:
+            print('Defective')
+            return 'Defective'
+        else:
+            print('Not Defective')
+            return 'Not Defective'
     
